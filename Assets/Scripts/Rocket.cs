118d882f8fb7a38dd9398ac74e40381ab067d3b0
Jamba <vincenzoilliano05@gmail.com>
2021-01-26 20:22:08 +0100

    Audio

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    [SerializeField] private float thrustForce;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private float stoppingAudioSpeed;
+
+    private Rigidbody _rigidbody;
+    private AudioSource _audio;
+
+    private void Start()
+    {
+        //initializers
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+            _audio = GetComponent<AudioSource>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //movment
+        {
+            ProcessInput();
+        }
+    }
+
+    private void ProcessInput()
+    {
+        //boost
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            _rigidbody.AddRelativeForce(Vector3.up * Time.deltaTime * thrustForce);
+
+            _audio.volume = 1;
+            if (!_audio.isPlaying)
+            {
+                _audio.Play();
+            }
+        }
+        else if(_audio.isPlaying)  //audio stop       
+        {
+            Tools.StopAudio(_audio, stoppingAudioSpeed);
+        }
+
+        //rotate
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * Time.deltaTime * rotationSpeed);
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * Time.deltaTime * rotationSpeed);
+        }
+    }
+
+}
