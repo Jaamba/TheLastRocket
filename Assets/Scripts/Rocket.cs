06b292c51e03160b0f3301c2ea8ec6eda4f349ad
Jamba <vincenzoilliano05@gmail.com>
2021-02-06 21:00:39 +0100

    BasicLevelDesign

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -1,13 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    //serializable parms
     [SerializeField] private float thrustForce;
     [SerializeField] private float rotationSpeed;
     [SerializeField] private float stoppingAudioSpeed;
+    [SerializeField] private float angularVelocityRange;
+    [SerializeField] private float maximumAngularVelocity;
 
+    //fields
     private Rigidbody _rigidbody;
     private AudioSource _audio;
 
@@ -18,6 +23,8 @@ public class Rocket : MonoBehaviour
             _rigidbody = GetComponent<Rigidbody>();
             _audio = GetComponent<AudioSource>();
         }
+
+        _rigidbody.maxAngularVelocity = maximumAngularVelocity;
     }
 
     // Update is called once per frame
@@ -25,11 +32,46 @@ public class Rocket : MonoBehaviour
     {
         //movment
         {
-            ProcessInput();
+            Thrust();
+            Rotate();
         }
     }
 
-    private void ProcessInput()
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch(collision.gameObject.tag)
+        {
+            case "friendly":
+                //print("friendly hit");
+                break;
+            default:
+                //print("you dead muahahahahah");
+                break;
+        }
+    }
+
+    //processes rotation input on the rocket
+    private void Rotate()
+    {
+        //rotate
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (_rigidbody.angularVelocity.z < angularVelocityRange)
+            {
+                _rigidbody.angularVelocity = new Vector3(_rigidbody.angularVelocity.x, _rigidbody.angularVelocity.y, _rigidbody.angularVelocity.z + (rotationSpeed * Time.deltaTime) );
+            }
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            if (_rigidbody.angularVelocity.z > -angularVelocityRange)
+            {
+                _rigidbody.angularVelocity = new Vector3(_rigidbody.angularVelocity.x, _rigidbody.angularVelocity.y, _rigidbody.angularVelocity.z - (rotationSpeed * Time.deltaTime));
+            }
+        }
+    }
+
+    //processes thrust input on the rocket
+    private void Thrust()
     {
         //boost
         if (Input.GetKey(KeyCode.Mouse0))
@@ -42,20 +84,9 @@ public class Rocket : MonoBehaviour
                 _audio.Play();
             }
         }
-        else if(_audio.isPlaying)  //audio stop       
+        else if (_audio.isPlaying)  //audio stop       
         {
             Tools.StopAudio(_audio, stoppingAudioSpeed);
         }
-
-        //rotate
-        if(Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(Vector3.forward * Time.deltaTime * rotationSpeed);
-        }
-        else if(Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(-Vector3.forward * Time.deltaTime * rotationSpeed);
-        }
     }
-
 }
